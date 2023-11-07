31e96b3ab1cea362fa10d2818003b84c62cd5a8f
Magfirah Salam <43084693+magfirahsalam@users.noreply.github.com>
2023-11-07 14:08:20 +0700

    Add files via upload

---
+++ Scripts/Categories/Step Definition/User input unavailable product name/Script1694110540182.groovy
@@ -0,0 +1,12 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory as MobileDriverFactory
+import io.appium.java_client.android.AndroidDriver as AndroidDriver
+import io.appium.java_client.android.AndroidKeyCode as AndroidKeyCode
+
+Mobile.tap(findTestObject('Categories/fieldsearch'), 5)
+
+Mobile.setText(findTestObject('Categories/fieldsearch'), unavailableproduct, 15)
+
+Mobile.waitForElementPresent(findTestObject('Categories/unavailableProduct'), 10)
+
